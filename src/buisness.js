7f993d2529c5853376adb9d40d488577d520e975
orbu13 <orelbutbul13@gmail.com>
2023-06-25 13:09:17 -0700

    run the 4th test and pass.

--- src/buisness.js
+++ src/buisness.js
@@ -14,25 +14,9 @@ export default class AgeCalculator {
     let mercuryAge = this.EarthAge() * 0.24;
     return mercuryAge;
   }
+
+  VenusAge() {
+    let venusAge = this.EarthAge() * 0.62;
+    return venusAge;
+  }
 }
-
-// Triangle.prototype.checkType = function () {
-//   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
-//     return "not a triangle";
-//   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
-//     return "scalene triangle";
-//   } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
-//     return "equilateral triangle";
-//   } else {
-//     return "isosceles triangle";
-//   }
-// };
-
-// Triangle.prototype.isBigTriangle = function () {
-//   const sum = this.side1 + this.side2 + this.side3;
-//   if (sum > 10) {
-//     return 'big';
-//   } else {
-//     return 'little';
-//   }
-// };
