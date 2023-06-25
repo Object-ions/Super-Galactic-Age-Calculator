24149b40abede7e74a0f98f59028c8d033eb774f
object-ions <moshikoatia@gmail.com>
2023-06-25 16:44:28 -0700

    will run the first test and pass

--- __tests__/TimePassed.test.js
+++ __tests__/TimePassed.test.js
@@ -7,10 +7,10 @@ describe('TimePassed', () => {
     timePassed = new TimePassed(31, 23);
   });
 
-  // test('should correctly create a time passed object with two instances', () => {
-  //   expect(timePassed.currentAge).toEqual(31);
-  //   expect(timePassed.pastAge).toEqual(23);
-  // });
+  test('should correctly create a time passed object with two instances', () => {
+    expect(timePassed.currentAge).toEqual(31);
+    expect(timePassed.pastAge).toEqual(23);
+  });
 
   
 });
