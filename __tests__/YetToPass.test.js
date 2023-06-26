d9681d9d7938aefe88d7a651c0067cb384ce47c9
object-ions <moshikoatia@gmail.com>
2023-06-25 17:47:43 -0700

    run the 1st test for 'YetToPass' and fail

---
+++ __tests__/YetToPass.test.js
@@ -0,0 +1,11 @@
+import YetToPass from '../src/YetToPass.js';
+
+describe('YetToPassed', () => {
+  let yetToPassed;
+  
+  beforeEach(() => {
+    yetToPassed = new YetToPassed(56, 61);
+  });
+
+  
+  });
---
+++ src/YetToPass.js
@@ -0,0 +1,5 @@
+export default class YetToPass {
+  constructor(currentAge, futureAge){
+    this.currentAge = currentAge;
+    this.futureAge = futureAge;
+  }
